1aedb6696bd05fa6daaf471d291b2a81c1f6d319
DaNaY111 <danaybroths@gmail.com>
2026-07-14 20:12:33 +0300

    feat: add round-robin load balancing

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,4 +1,7 @@
-use std::io::Result;
+use std::{
+    io::Result,
+    sync::{Arc, atomic::AtomicUsize},
+};
 
 use tokio::net::TcpListener;
 
@@ -8,14 +11,27 @@ mod proxy;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let backends_addr = vec![
+        "localhost:9000".to_string(),
+        "localhost:9001".to_string(),
+        "localhost:9002".to_string(),
+    ];
+
+    let backends = Arc::new(backends_addr);
+
+    let counter = Arc::new(AtomicUsize::new(0));
+
     let listener = TcpListener::bind("127.0.0.1:8081").await?;
     println!("Proxy started on port 8081");
 
     loop {
         let (socket, _addr) = listener.accept().await?;
 
+        let backends_clone = Arc::clone(&backends);
+        let counter_clone = Arc::clone(&counter);
+
         tokio::spawn(async move {
-            if let Err(e) = handle_connection(socket).await {
+            if let Err(e) = handle_connection(socket, backends_clone, counter_clone).await {
                 eprintln!("Error client handling: {}", e)
             }
         });
--- proxy/src/proxy.rs
+++ proxy/src/proxy.rs
@@ -1,30 +1,60 @@
-use std::io::Result;
+use std::{
+    io::Result,
+    sync::{
+        Arc,
+        atomic::{AtomicUsize, Ordering},
+    },
+};
 
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
 
-pub async fn handle_connection(mut client_socket: TcpStream) -> Result<()> {
-  let mut client_buf = [0; 1024];
-  let n = client_socket.read(&mut client_buf).await?;
-  if n == 0 { return Ok(()) }
+pub async fn handle_connection(
+    mut client_socket: TcpStream,
+    backends: Arc<Vec<String>>,
+    counter: Arc<AtomicUsize>,
+) -> Result<()> {
+    let mut client_buf = [0; 1024];
+    let n = client_socket.read(&mut client_buf).await?;
+    if n == 0 {
+        return Ok(());
+    }
 
-  println!("Recieved: \n {}", String::from_utf8_lossy(&client_buf[..n]));
+    println!("Recieved: \n {}", String::from_utf8_lossy(&client_buf[..n]));
 
-  let backend_addr = "127.0.0.1:9000";
-  let mut backend_socket = TcpStream::connect(backend_addr).await?;
+    let current_count = counter.fetch_add(1, Ordering::SeqCst);
+    println!("CURRENT_COUNT: {}", current_count);
+    let index = current_count % backends.len();
+    let backend_addr = &backends[index];
 
-  backend_socket.write_all(&client_buf[..n]).await?;
+    println!(
+        "--> Перенаправляем запрос на бэкенд [{}]: {}",
+        index, backend_addr
+    );
 
-  let mut backend_buf = [0; 4096];
+    let mut backend_socket = TcpStream::connect(backend_addr).await?;
 
-  loop {
-      let bytes_read = backend_socket.read(&mut backend_buf).await?;
+    backend_socket.write_all(&client_buf[..n]).await?;
 
-      if bytes_read == 0 {
-          break;
-      }
+    let mut backend_buf = [0; 4096];
 
-      client_socket.write_all(&backend_buf[..bytes_read]).await?;
-  }
+    loop {
+        let bytes_read = backend_socket.read(&mut backend_buf).await?;
 
-  Ok(())
+        if bytes_read == 0 {
+            break;
+        }
+
+        println!(
+            "=== Ответ от сервера {} ===\n{}",
+            backend_addr,
+            String::from_utf8_lossy(&backend_buf[..bytes_read])
+        );
+
+        client_socket.write_all(&backend_buf[..bytes_read]).await?;
+    }
+
+    Ok(())
 }
